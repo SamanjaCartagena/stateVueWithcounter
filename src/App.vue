ed2e5c031489ce47c3b2c10a71a97f02fcb38689
SamanjaCartagena <cart.samanja09@gmail.com>
2022-04-19 22:57:20 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <base-container :textOfTitle="name">
+   <h3>{{counter}}</h3>
+   <button @click="addOne">Add</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  data(){
+   return{
+    name:'Samanja'
+   }
+  },
+  components: {
+    BaseContainer,
+  },
+  computed:{
+    counter(){
+      return this.$store.state.counter;
+    }
+  },
+  methods:{
+    addOne(){
+       this.$store.state.counter++;
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
